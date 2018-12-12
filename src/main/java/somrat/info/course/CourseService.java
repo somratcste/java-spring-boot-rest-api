52e17654e4445f9cef43787b9a39e380a1af7e37
somratcste <somratcste@gmail.com>
2018-12-12 14:42:06 +0600

    rest api

---
+++ src/main/java/somrat/info/course/CourseService.java
@@ -0,0 +1,38 @@
+package somrat.info.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	public List<Course> getAllCourses(String topicId) {
+		List<Course> topics = new ArrayList<>();
+		courseRepository.findByTopicId(topicId).forEach(topics::add);
+		return topics;
+	}
+	
+	public Optional<Course> getCourse(String id) {
+		return courseRepository.findById(id);
+	}
+
+	public void addCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.deleteById(id);
+	}
+}
